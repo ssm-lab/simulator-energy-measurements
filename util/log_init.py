b8b24cfe641a78b2cac6b3adfc11ee5d741057d6
YimoNing <yimoning@mail.com>
2023-12-06 19:01:52 -0500

    modified logging_init script

--- util/log_init.py
+++ util/log_init.py
@@ -1,10 +1,21 @@
 import logging
+import os
 
 
 def log_init():
+    # Determine the root directory of the project
+    project_root_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Set the base directory for logging
+    base_log_dir = os.path.join(project_root_dir, 'logging')
+    info_log_dir = os.path.join(base_log_dir, 'info')
+    error_log_dir = os.path.join(base_log_dir, 'error')
+
+    # Create directories if they don't exist
+    os.makedirs(info_log_dir, exist_ok=True)
+    os.makedirs(error_log_dir, exist_ok=True)
+
     log_level = logging.DEBUG
-    log_file_path = '/home/yimoning/mcmaster/fall2023/new_simMeasure/_remove/logging/info/'  # Set your log file path here
-    log_error_file_path = '/home/yimoning/mcmaster/fall2023/new_simMeasure/_remove/logging/error/'  # Set your error log file path here
     enable_stdout_log = True
     log_format_str = "[%(asctime)s][%(levelname)s][%(process)d][%(module)s.%(funcName)s] %(message)s"
     formatter = logging.Formatter(log_format_str)
@@ -15,11 +26,11 @@ def log_init():
         stdout_handle.setFormatter(formatter)
         logging.getLogger().addHandler(stdout_handle)
 
-    log_handle = logging.FileHandler(log_file_path + 'info.log')
+    log_handle = logging.FileHandler(os.path.join(info_log_dir, 'info.log'))
     log_handle.setFormatter(formatter)
     log_handle.setLevel(log_level)
 
-    error_handle = logging.FileHandler(log_error_file_path + 'error.log')
+    error_handle = logging.FileHandler(os.path.join(error_log_dir, 'error.log'))
     error_handle.setFormatter(formatter)
     error_handle.setLevel(logging.WARNING)
 
